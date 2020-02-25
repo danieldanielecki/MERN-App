a1f094b91ab3543668c7eff2b4555dcf99e46027
Daniel Danielecki <daniel.danielecki@mail.com>
2020-02-25 12:10:27 +0100

    Add: basic Express routes

--- server.js
+++ server.js
@@ -7,4 +7,11 @@ const PORT = process.env.PORT || 5000;
 connectDB(); // Connect database.
 
 app.get("/", (req, res) => res.send("API Running"));
+
+// Define routes.
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

562275d42784c9bc21638a3a321391958208ca2a
Daniel Danielecki <daniel.danielecki@mail.com>
2020-03-30 09:53:17 +0200

    Add: request & body validation

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const PORT = process.env.PORT || 5000;
 
 connectDB(); // Connect database.
 
+app.use(express.json()); // Initialize middleware.
+
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes.
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/users', require('./routes/api/users'));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
